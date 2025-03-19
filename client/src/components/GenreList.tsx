f7c99d4c67db34deef7139236cc1d07189ba02bb
ogeid13 <diego.rodr.gonz.96@gmail.com>
2025-03-19 01:30:16 -0600

    Genres Skeleton

--- client/src/components/GenreList.tsx
+++ client/src/components/GenreList.tsx
@@ -1,25 +1,30 @@
 import { HStack, Image, Text, VStack } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import SkeletonGenre from "./SkeletonGenre";
 
 function GenreList() {
   const { data, error, isLoading } = useGenre();
+  const skeletons = Array<number>(15).fill(0);
 
+  if (error) return null;
   return (
     <VStack alignItems="left">
       <Text fontSize={"3xl"} marginBottom={5}>
         Genres
       </Text>
-      {data.map((g) => (
-        <HStack key={g.id}>
-          <Image
-            src={g.image_background}
-            height="40px"
-            width="40px"
-            borderRadius={10}
-          ></Image>
-          <Text>{g.name}</Text>
-        </HStack>
-      ))}
+      {isLoading && skeletons.map((i, index) => <SkeletonGenre key={index} />)}
+      {!isLoading &&
+        data.map((g) => (
+          <HStack key={g.id}>
+            <Image
+              src={g.image_background}
+              height="40px"
+              width="40px"
+              borderRadius={10}
+            ></Image>
+            <Text>{g.name}</Text>
+          </HStack>
+        ))}
     </VStack>
   );
 }
---
+++ client/src/components/SkeletonGenre.tsx
@@ -0,0 +1,17 @@
+import { HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+function SkeletonGenre() {
+  return (
+    <HStack>
+      <Skeleton height="40px" width="40px" borderRadius={10}></Skeleton>
+      <SkeletonText
+        noOfLines={1}
+        skeletonHeight="2"
+        height="10px"
+        width="65%"
+      ></SkeletonText>
+    </HStack>
+  );
+}
+
+export default SkeletonGenre;
